3fc5ddd90df05cd12655eaf18a29a8bee0e793f0
TouSS <dxiaoxiang@gmail.com>
2018-10-31 16:15:17 +0800

    Timeline Add ...

---
+++ src/components/timeline/index.js
@@ -0,0 +1,83 @@
+import Timeline from './src/timeline.vue'
+
+if (typeof window !== 'undefined' && window.Vue) {
+  Vue.component(Timeline.name, Timeline)
+  window.app = new Vue({
+    el: '#app',
+    data: {
+      events: [
+        {
+          year: '2008',
+          eventList: [
+            {
+              month: '二月',
+              day: '13',
+              eventList: [
+                {
+                  title: 'Philadelphia Museum School',
+                  period: '10:00 - 11:00',
+                  event:
+                    "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant."
+                }
+              ]
+            },
+            {
+              month: '三月',
+              day: '1',
+              eventList: [
+                {
+                  title: 'Philadelphia Museum School',
+                  period: '10:00 - 11:00',
+                  event:
+                    "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant."
+                },
+                {
+                  title: 'Philadelphia Museum School',
+                  period: '16:00 - 18:00',
+                  event:
+                    "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant."
+                }
+              ]
+            }
+          ]
+        },
+        {
+          year: '2009',
+          eventList: [
+            {
+              month: '二月',
+              day: '1',
+              eventList: [
+                {
+                  title: 'Philadelphia Museum School',
+                  period: '10:00 - 11:00',
+                  event:
+                    "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant."
+                }
+              ]
+            },
+            {
+              month: '五月',
+              day: '1',
+              eventList: [
+                {
+                  title: 'Philadelphia Museum School',
+                  period: '10:00 - 11:00',
+                  event:
+                    "Attends the Philadelphia Museum School of Industrial Art. Studies design with Alexey Brodovitch, art director at Harper's Bazaar, and works as his assistant."
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    methods: {
+      onChoose(detail) {
+        console.log(detail)
+      }
+    }
+  })
+}
+
+export default Timeline
---
+++ src/components/timeline/src/timeline.vue
@@ -0,0 +1,157 @@
+<template>
+  <div class="timeline">
+    <div class="timeline__group" v-for="y in events" :key="y.year">
+      <span class="timeline__year">{{y.year}}</span>
+      <div class="timeline__box" v-for="d in y.eventList" :key="y.year + '-' + d.month + '-' + d.day">
+        <div class="timeline__date">
+          <span class="timeline__day">{{d.day}}</span>
+          <span class="timeline__month">{{d.month}}</span>
+        </div>
+        <div class="timeline__post">
+          <div class="timeline__content" v-for="e in d.eventList" :key="e.period">
+            <h3 @click="onChoose(y.year, d.month, d.day, e)">{{e.title}}</h3>
+            <h4>{{e.period}}</h4>
+            <p>{{e.event}}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+export default {
+  name: 'timeline',
+  label: '时间轴',
+  props: ['events'],
+  methods: {
+    onChoose(year, month, day, event) {
+      this.$emit('on-choose', {year: year, month: month, day: day, title: event.title, period: event.period, detail: event.event})
+    }
+  }
+}
+</script>
+<style scoped>
+.timeline {
+  --uiTimelineMainColor: var(--timelineMainColor, #222);
+  --uiTimelineSecondaryColor: var(--timelineSecondaryColor, #fff);
+
+  position: relative;
+  padding-top: 3rem;
+  padding-bottom: 3rem;
+}
+
+.timeline:before {
+  content: '';
+  width: 4px;
+  height: 100%;
+  background-color: var(--uiTimelineMainColor);
+
+  position: absolute;
+  top: 0;
+}
+
+.timeline__group {
+  position: relative;
+}
+
+.timeline__group:not(:first-of-type) {
+  margin-top: 4rem;
+}
+
+.timeline__year {
+  padding: 0.5rem 1.5rem;
+  color: var(--uiTimelineSecondaryColor);
+  /* background-color: var(--uiTimelineMainColor); */
+  background-color:#131e5e;
+
+  position: absolute;
+  left: 0;
+  top: 0;
+}
+
+.timeline__box {
+  position: relative;
+}
+
+.timeline__box:not(:last-of-type) {
+  margin-bottom: 30px;
+}
+
+.timeline__box:before {
+  content: '';
+  width: 100%;
+  height: 2px;
+  background-color: var(--uiTimelineMainColor);
+
+  position: absolute;
+  left: 0;
+  z-index: -1;
+}
+
+.timeline__date {
+  min-width: 70px;
+  position: absolute;
+  left: 0;
+
+  box-sizing: border-box;
+  padding: 0.5rem 0;
+  text-align: center;
+
+  background-color: var(--uiTimelineMainColor);
+  color: var(--uiTimelineSecondaryColor);
+}
+
+.timeline__day {
+  font-size: 2rem;
+  font-weight: 700;
+  display: block;
+}
+
+.timeline__month {
+  display: block;
+  font-size: 0.8em;
+  text-transform: uppercase;
+}
+
+.timeline__post {
+  padding: 1.5rem 2rem;
+  border-radius: 2px;
+  border-left: 3px solid var(--uiTimelineMainColor);
+  box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.12), 0 1px 2px 0 rgba(0, 0, 0, 0.24);
+  background-color: var(--uiTimelineSecondaryColor);
+  margin-left: 2rem;
+}
+
+.timeline:before {
+  left: 30px;
+}
+
+.timeline__group {
+  padding-top: 55px;
+}
+
+.timeline__box {
+  padding-left: 80px;
+}
+
+.timeline__box:before {
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+.timeline__date {
+  top: 50%;
+  margin-top: -27px;
+}
+
+.timeline {
+  --timelineMainColor: #4557bb;
+}
+
+.timeline__content h3 {
+  cursor: pointer;
+  color: #5565c0;
+}
+
+</style>
